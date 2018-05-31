79e7035963cac7cec43af7d086dc831f803e4445
Gonzalo Palavesino <gabriel_gpx@live.com>
2018-05-31 02:26:16 -0300

    New

---
+++ Sources/Trabajo_Practico_1_Consigna 2/Debug/src/Trabajo_Practico_1_Consigna 2.d
@@ -0,0 +1,2 @@
+src/Trabajo_Practico_1_Consigna\ 2.d: \
+ ../src/Trabajo_Practico_1_Consigna\ 2.cpp
---
+++ Sources/Trabajo_Practico_1_Consigna 2/src/Trabajo_Practico_1_Consigna 2.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Trabajo_Practico_1_Consigna.cpp
+// Author      : Gonzalo Palavesino
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Welcome" << endl; // prints Welcome
+	return 0;
+}
